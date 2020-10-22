0c39101448b8383e685b6dd9f3dc556934d48ff2
Praveen Bisht <prvnbist@gmail.com>
2020-10-22 15:40:46 +0530

    feat: added & renamed apps

--- js/data.js
+++ js/data.js
@@ -1,131 +1,133 @@
 var desktop_icons = [
   {
     id: "settings",
-    title: "Settings App",
+    title: "Settings",
     image: "img/settings.png",
     win: "settings_win",
   },
   {
     id: "inventory",
-    title: "Inventory App",
+    title: "Inventory",
     image: "img/inventory.png",
     win: "inventory_win",
   },
   {
-    id: "recipe",
-    title: "Recipe App",
-    image: "img/recipe.png",
-    win: "recipe_win",
+    id: "product",
+    title: "Product",
+    image: "img/product.png",
+    win: "product_win",
   },
   {
-    id: "store",
-    title: "Store App",
-    image: "img/store.png",
-    win: "store_win",
+    id: "menu",
+    title: "Menu",
+    image: "img/menu.png",
+    win: "menu_win",
   },
   {
     id: "order",
-    title: "Order App",
+    title: "Order",
     image: "img/order.png",
     win: "order_win",
   },
   {
     id: "safety",
-    title: "Safety App",
+    title: "Safety",
     image: "img/safety.png",
     win: "safety_win",
   },
   {
     id: "crm",
-    title: "CRM App",
+    title: "CRM",
     image: "img/crm.png",
     win: "crm_win",
   },
   {
     id: "subscription",
-    title: "Subscription App",
+    title: "Subscription",
     image: "img/subscription.png",
     win: "subscription_win",
+  },
+  {
+    id: "brand",
+    title: "Brand",
+    image: "img/brand.png",
+    win: "brand_win",
+  },
+  {
+    id: "insights",
+    title: "Insights",
+    image: "img/insights.png",
+    win: "insights_win",
   },
 ];
 
 var startmenu_icons = [
   {
     id: "settings",
-    title: "Settings App",
+    title: "Settings",
     image: "img/settings.png",
     win: "settings_win",
   },
   {
     id: "inventory",
-    title: "Inventory App",
+    title: "Inventory",
     image: "img/inventory.png",
     win: "inventory_win",
   },
   {
-    id: "recipe",
-    title: "Recipe App",
-    image: "img/recipe.png",
-    win: "recipe_win",
+    id: "product",
+    title: "Product",
+    image: "img/product.png",
+    win: "product_win",
   },
   {
-    id: "store",
-    title: "Store App",
-    image: "img/store.png",
-    win: "store_win",
-  },
-  {
-    id: "support",
-    title: "Support App",
-    image: "img/support.png",
-    win: "support_win",
+    id: "menu",
+    title: "Menu",
+    image: "img/menu.png",
+    win: "menu_win",
   },
   {
     id: "order",
-    title: "Order App",
+    title: "Order",
     image: "img/order.png",
     win: "order_win",
   },
   {
     id: "safety",
-    title: "Safety App",
+    title: "Safety",
     image: "img/safety.png",
     win: "safety_win",
   },
   {
     id: "crm",
-    title: "CRM App",
+    title: "CRM",
     image: "img/crm.png",
     win: "crm_win",
   },
   {
     id: "subscription",
-    title: "Subscription App",
+    title: "Subscription",
     image: "img/subscription.png",
     win: "subscription_win",
+  },
+  {
+    id: "brand",
+    title: "Brand",
+    image: "img/brand.png",
+    win: "brand_win",
+  },
+  {
+    id: "insights",
+    title: "Insights",
+    image: "img/insights.png",
+    win: "insights_win",
   },
 ];
 
 var start_menu = {
   id: "1",
   value: "Start",
-  submenu: ["File Manager"],
 };
 
-var winmenu_1 = [
-  //   {
-  //     value: "Email",
-  //     id: "email",
-  //     icon: "email",
-  //     color: "#297acc",
-  //     width: 2,
-  //     x: 2,
-  //     y: 1,
-  //   },
-];
+var winmenu_1 = [];
 var winmenu_2 = [];
-
-/*
-var test_data_set_2015 =
-  "<data><event id='1261150506' start_date='2015-08-01 18:00' end_date='2015-08-03 18:00' text='Sister Act' details='Palladium' /><event id='1261150507' start_date='2015-08-02 07:00' end_date='2015-08-02 09:00' text='Cat On a Hot Tin Roof' details='Novello Theatre' /><event id='1261150508' start_date='2015-08-02 07:00' end_date='2015-08-02 09:00' text='Grease' details='Piccadilly Theatre' /><event id='1261150509' start_date='2015-08-03 17:00' end_date='2015-08-03 19:00' text='Ladbrokes.com World Darts Championships' details='Alexandra Palace' /><event id='1261150510' start_date='2015-08-05 14:00' end_date='2015-08-05 16:00' text='Calendar Girls' details='Noel Coward Theatre' /><event id='1261150511' start_date='2015-08-06 08:00' end_date='2015-08-08 08:00' text='Dirty Dancing' details='Aldwych Theatre' /><event id='1261150512' start_date='2015-08-07 15:00' end_date='2015-08-07 17:00' text='Cirque du Soleil Varekai' details='Royal Albert Hall' /><event id='1261150513' start_date='2015-08-08 15:00' end_date='2015-08-08 17:00' text='Grease' details='Piccadilly Theatre' /><event id='1261150514' start_date='2015-08-09 09:00' end_date='2015-08-09 11:00' text='The Lion King' details='Lyceum Theatre' /><event id='1261150515' start_date='2015-08-09 07:00' end_date='2015-08-09 09:00' text='Cirque du Soleil Varekai' details='Royal Albert Hall' /><event id='1261150516' start_date='2015-08-10 10:00' end_date='2015-08-12 10:00' text='Cirque du Soleil Varekai' details='Royal Albert Hall' /><event id='1261150517' start_date='2015-08-12 09:00' end_date='2015-08-12 11:00' text='Masters Snooker 2015      Afternoon session' details='Wembley Arena' /></data>";
-  */
